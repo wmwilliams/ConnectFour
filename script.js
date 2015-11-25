efd58c68a5035cd75c5b6e139c8c4a45cd47a82b
Wyatt Williams <williams.wyatt@gmail.com>
2015-11-24 23:30:56 -0800

    no local storage yet, but added function to increment and post player wins to the dom

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
+//Hopefully ill get this to load last games names and scores....
+// $("#load").on('click', function() {
+
+// })
 //Takes player names from modal form and clears form after
+var p1wins = 0,
+	p2wins = 0,
+	whosUp,
+	moveCount = 0,
+	board = [
+	[0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0],
+];
+
 $('#playerEntry').on("click", function() {
 	$('#p1board').text($('#firstPlayer').val()); 
 	$('#p2board').text($('#secondPlayer').val()); 
+	$('#p1wins').text(p1wins),
+	$('#p2wins').text(p2wins),
 	$('.modalForm').find('form')[0].reset();
 });
 $('#reset').on('click', function() {
@@ -17,16 +36,6 @@ $('#reset').on('click', function() {
 	var moveCount = 0;
 });
 //When the board is clicked.......
-var whosUp;
-var moveCount = 0;
-var board = [
-	[0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0],
-];
 $("ul.columns").on("click", function EventHandler(e) {
 	e = e;
 	var target = (e.target.tagName === "IMG" ? e.target.closest('li') : e.target);
@@ -50,58 +59,72 @@ $("ul.columns").on("click", function EventHandler(e) {
 		}
 	}
 });
+var score = function() {
+	if(moveCount % 2 !== 0) {
+		moveCount = 0;
+		alert("It looks like player 1 has won.");
+		p1wins++;
+		$('#p1wins').text(p1wins);
+	} else {
+		moveCount = 0;
+		alert("It looks like player 2 has won.");
+		p2wins++;
+		$('#p2wins').text(p2wins);
+	}
+};
+
 var checkWinner = function(array,i,j) {
 //check vertical wins
 	var i = parseInt(i),
 		j = parseInt(j);
 //check vertical wins
 	if(j >= 3 && array[i][j] !== 0 && array[i][j] === array[i][j-1] && array[i][j-1] === array[i][j-2] && array[i][j-2] === array[i][j-3]) {
-		alert("It looks like player "+whosUp+" is the winner!");
+		score();
 	} //check horizontal wins
 	  //should check horizontally to the left of piece played
 	else if(i >= 3 && array[i][j] !== 0 && array[i][j] === array[i-1][j] && array[i-1][j] === array[i-2][j] && array[i-2][j] === array[i-3][j]) {
-		alert("Winner to the left!" );
+		score();
 	} //check to the right of piece played
 	else if(i <= 2 && array[i][j] !== 0 && array[i][j] === array[i+1][j] && array[i+1][j] === array[i+2][j] && array[i+2][j] === array[i+3][j]) {
-		alert("Winner to the right!" )
+		score();
 	} //check 2 to the right and one to the left
 	else if(i >=1 && i <= 3 && array[i][j] !== 0 && array[i][j] === array[i+1][j] && array[i+1][j] === array[i+2][j] && array[i+2][j] === array[i-1][j]) {
-		alert("Winner 2r-1l")
+		score();
 	} //check 2 to the left and one to the right
 	else if (i >= 2 && i <= 4 && array[i][j] !== 0 && array[i][j] === array[i-1][j] && array[i-1][j] === array[i-2][j] && array[i-2][j] === array[i+1][j]) {
-		alert("winner 2l-1r");
+		score();
 	} //Begin Diagonal
 	  //Checks three to the NE
 	else if (i <= 2 && j <= 2 && array[i][j] !== 0 && array[i][j] === array[i+1][j+1] && array[i+1][j+1] === array[i+2][j+2] && array[i+2][j+2] === array[i+3][j+3]) {
-		alert("winner d-up-to-right");
+		score();
 	}
 	 //Check three to the NW
 	else if (i >= 3 && j <= 2 && array[i][j] !== 0 && array[i][j] === array[i-1][j+1] && array[i-1][j+1] === array[i-2][j+2] && array[i-2][j+2] === array[i-3][j+3]) {
-		alert("winner d-up-to-left");
+		score();
 	}
 	 //Check three to the SW
 	else if (i >= 3 && j >= 3 && array[i][j] !== 0 && array[i][j] === array[i-1][j-1] && array[i-1][j-1] === array[i-2][j-2] && array[i-2][j-2] === array[i-3][j-3]) {
-		alert("winner d-down-to-the-left");
+		score();
 	} 
 	//Check three to the SE
 	else if (i <= 2 && j >= 3 && array[i][j] !== 0 && array[i][j] === array[i+1][j-1] && array[i+1][j-1] === array[i+2][j-2] && array[i+2][j-2] === array[i+3][j-3]) {
-		alert("winner d-down-to-the-right");
+		score();
 	} 
 	//Check 2 SE & 1 NW
 	else if (i <= 3 && i >= 1 && j <= 4 && j >= 2 && array[i][j] !== 0 && array[i][j] === array[i-1][j+1] && array[i-1][j+1] === array[i+1][j-1] && array[i+1][j-1] === array[i+2][j-2]) {
-		alert("winner d-2DR-1UL");
+		score();
 	}
 	//Check 1 SE & 2 NW
 	else if (i >= 2 && i <= 4 && j >= 1 && j <= 3 && array[i][j] !== 0 && array[i][j] === array[i+1][j-1] && array[i+1][j-1] === array[i-1][j+1] && array[i-1][j+1] === array[i-2][j]) {
-		alert("winner d-1DR-2UL");
+		score();
 	}
 	//Check 1 SW & 2 NE
 	else if(i >= 1 && i <=3 && j >= 1 && j <= 3 && array[i][j] !== 0 && array[i][j] === array[i-1][j-1] && array[i-1][j-1] === array[i+1][j+1] && array[i+1][j+1] === array[i+2][j+2]) {
-		alert("winner d-1DL-2UR");
+		score();
 	}
 	//Check 2 SW & 1 NE
 	else if(i >= 2 && i <= 4 && j >= 2 && j <= 4 && array[i][j] !== 0 && array[i][j] === array[i+1][j+1] && array[i+1][j+1] === array[i-1][j-1] && array[i-1][j-1] === array[i-2][j-2]) {
-		alert('winner d-2DL-1UR');
+		score();
 	}
 	 else {
 		console.log("NO winner yet");
